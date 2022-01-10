af8cf2e4454aed760c4c4cdfa79eed017a7041d0
Rishabh Gupta <82943941+Rishabh2804@users.noreply.github.com>
2022-01-10 15:20:55 +0530

    Added avatar and Name label

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,41 @@ class FlutteryChatApp extends StatelessWidget {
   }
 }
 
+class ChatMessage extends StatelessWidget {
+  const ChatMessage({
+    required this.text,
+    Key? key,
+  }) : super(key: key);
+
+  final String text;
+  final String _name = 'Rishabh';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: CircleAvatar(child: Text(_name[0])),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(_name, style: Theme.of(context).textTheme.headline4),
+                Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: Text(text),
+                ),
+              ],
+            )
+          ],
+        ));
+  }
+}
+
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
@@ -33,18 +68,37 @@ class _ChatScreenState extends State<ChatScreen> {
 
   void _handleSubmitted(String text) {
     _textController.clear();
-    
   }
- 
+
   Widget _buildTextComposer() {
-    return Container(
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).colorScheme.secondary),
+      child: Container(
         margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: TextField(
-          controller: _textController,
-          onSubmitted: _handleSubmitted,
-          decoration:
-              const InputDecoration.collapsed(hintText: 'Send a message'),
-        ));
+        child: Row(
+          children: [
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    const InputDecoration.collapsed(hintText: 'Send a message'),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: const Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text),
+                // hoverColor: Colors.accents[11],
+                // focusColor: Colors.cyan,
+                // color: Colors.accents[8],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   @override
