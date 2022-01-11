e1dc0a50db703c6e0e2ccf4d3218e6ac5181dcc9
Rishabh Gupta <82943941+Rishabh2804@users.noreply.github.com>
2022-01-11 22:57:17 +0530

    Alignment and accessibility changes in UI

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class FlutteryChatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      scrollBehavior: MaterialScrollBehavior(),
       debugShowCheckedModeBanner: false,
       title: 'FlutteryChat',
       home: ChatScreen(),
@@ -28,31 +29,33 @@ class ChatMessage extends StatelessWidget {
   }) : super(key: key);
 
   final String text;
-  final String _name = 'Rishabh';
+  final String _name = 'You';
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: const EdgeInsets.symmetric(vertical: 10.0),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(right: 16.0),
-              child: CircleAvatar(child: Text(_name[0])),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(_name, style: Theme.of(context).textTheme.headline4),
-                Container(
-                  margin: const EdgeInsets.only(top: 5.0),
-                  child: Text(text),
-                ),
-              ],
-            )
-          ],
-        ));
+      margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(_name[0])),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(_name, style: Theme.of(context).textTheme.headline4),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Text(text),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
 
@@ -64,10 +67,47 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = [];
   final _textController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
 
   void _handleSubmitted(String text) {
+    if (text.isEmpty) {
+      _focusNode.requestFocus();
+      return;
+    }
     _textController.clear();
+    var message = ChatMessage(
+      text: text,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+    _focusNode.requestFocus();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('FlutteryChat')),
+      body: Column(
+        children: [
+          Flexible(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_, index) => _messages[index],
+              itemCount: _messages.length,
+            ),
+          ),
+          const Divider(height: 1.0),
+          Container(
+            decoration: BoxDecoration(color: Theme.of(context).cardColor),
+            child: _buildTextComposer(),
+          ),
+        ],
+      ),
+    );
   }
 
   Widget _buildTextComposer() {
@@ -79,35 +119,36 @@ class _ChatScreenState extends State<ChatScreen> {
           children: [
             Flexible(
               child: TextField(
+                mouseCursor: MaterialStateMouseCursor.textable,
                 controller: _textController,
+                scrollController: ScrollController(),
                 onSubmitted: _handleSubmitted,
                 decoration:
                     const InputDecoration.collapsed(hintText: 'Send a message'),
+                keyboardType: TextInputType.multiline,
+                textInputAction: TextInputAction.newline,
+                autofocus: true,
+                focusNode: _focusNode,
               ),
             ),
             Container(
-              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              margin:
+                  const EdgeInsets.symmetric(horizontal: 4.0, vertical: 6.0),
               child: IconButton(
+                visualDensity: VisualDensity.comfortable,
                 icon: const Icon(Icons.send),
                 onPressed: () => _handleSubmitted(_textController.text),
                 // hoverColor: Colors.accents[11],
+                // autofocus: true,
+                mouseCursor: MaterialStateMouseCursor.clickable,
                 // focusColor: Colors.cyan,
-                // color: Colors.accents[8],
+                color: Colors.cyan[600],
+                // splashColor: Colors.tealAccent,
               ),
             )
           ],
         ),
       ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('FlutteryChat'),
-      ),
-      body: _buildTextComposer(),
     );
   }
 }
